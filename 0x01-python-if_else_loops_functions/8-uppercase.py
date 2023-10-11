3d1cddcba86ab82d6221b9604bff4a987ae35c2e
Pilate <pilatech6@gmail.com>
2023-10-11 09:46:54 +0200

    add #7&#8 of 0x01

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if (ord(str[i]) >= 97) and (ord(str[i]) <= 122):
+            code = ord(str[i]) - 32
+        else:
+            code = ord(str[i])
+        print(chr(code), end="")
+    print("")
