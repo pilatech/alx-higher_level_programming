69b18f2eaf5da6dfe558b764a5ef2f74045d0982
Pilate <pilatech6@gmail.com>
2023-10-11 07:00:59 +0200

    add #2&#3 of 0x01

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+for code in range(97, 123):
+    print(chr(code), end="")
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+for code in range(97, 123):
+    if (code == 101) or (code == 113):
+        continue
+    print(chr(code), end="")
