5d1987ac062757a861208c14f7da947fe90b893e
Pilate <pilatech6@gmail.com>
2023-10-18 17:16:41 +0200

    edit the 0 case on 0x02

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     if len(argv) == 1:
-        print("0 argument.")
+        print("0 arguments.")
     elif len(argv) == 2:
         print("1 argument:")
     else:
