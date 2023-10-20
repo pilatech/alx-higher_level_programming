4f7c0ba8c269ef84c83e848fe49e9e1c9003720b
Pilate <pilatech6@gmail.com>
2023-10-20 07:48:38 +0200

    add #9 of 0x03

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    mx = my_list[0]
+    for item in my_list:
+        if item > mx:
+            mx = item
+    return mx
