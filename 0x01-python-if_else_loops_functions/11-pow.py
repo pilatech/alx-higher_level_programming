ff1d50abb63a29ab0d22206f24c290a91de8e061
Pilate <pilatech6@gmail.com>
2023-10-22 20:24:53 +0200

    edit #11 of 0x01

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if (b == 0):
-        return 1
-
-    product = 1
-    i = 0
-    if (b < 0):
-        while i < -b:
-            product *= (1 / a)
-            i += 1
-    else:
-        while i < b:
-            product *= a
-            i += 1
-    return product
+    return (a ** b)
