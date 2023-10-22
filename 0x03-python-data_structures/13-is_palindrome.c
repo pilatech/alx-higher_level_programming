5e71e2faf8ccd90e3a3dd7fd37894777d562f1de
Pilate <pilatech6@gmail.com>
2023-10-22 20:14:30 +0200

    add #13 of 0x03

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,68 +1,69 @@
 #include "lists.h"
 
 /**
+ * reverse_listint - Reverses a singly-linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
+ *
+ * Return: A pointer to the head of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+
+/**
  * is_palindrome - check if a linked list is a palindrome.
  * @head: the start of the list.
  *
  * Return: 1 if it's palindrom. otherwise return 0.
  */
+
 int is_palindrome(listint_t **head)
 {
-	int len = 0;
-	int idx = 0;
-	int count = 1;
-	listint_t *temp;
-	listint_t *forward;
+	listint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
 
-	if (*head == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	temp = *head;
-	while (temp != NULL)
+
+	tmp = *head;
+	while (tmp)
 	{
-		len++;
-		temp = temp->next;
+		size++;
+		tmp = tmp->next;
 	}
-	if ((len % 2) != 0)
+
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
+
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
 		return (0);
-	if (len == 2)
+
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
+
+	tmp = *head;
+	while (rev)
 	{
-		if ((*head)->n != (*head)->next->n)
+		if (tmp->n != rev->n)
 			return (0);
+		tmp = tmp->next;
+		rev = rev->next;
 	}
-	temp = *head;
-	while (count != (len / 2) + 1)
-	{
-		temp = temp->next;
-		count++;
-	}
-	forward = temp;
-	while (forward != NULL)
-	{
-		if (forward->n != get_curr_back(*head, len, idx)->n)
-			return (0);
-		forward = forward->next;
-		idx++;
-	}
+	reverse_listint(&mid);
+
 	return (1);
-}
-
-/**
- * get_curr_back - get current node moving backwards.
- * @head: the start of the list.
- * @len: length of the list.
- * @idx: the index of item from the center.
- *
- * Return: current node moving backward.
- */
-listint_t *get_curr_back(listint_t *head, int len, int idx)
-{
-	int count = 1;
-	listint_t *curr_back = head;
-
-	while (count != (len / 2) - idx)
-	{
-		curr_back = curr_back->next;
-		count++;
-	}
-	return (curr_back);
 }
