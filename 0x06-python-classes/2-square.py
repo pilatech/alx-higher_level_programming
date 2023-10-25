496beaa79f6ce3460f0d8c30f36ff95a3c62f373
Pilate <pilatech6@gmail.com>
2023-10-25 02:56:43 +0200

    add #2 of 0x06

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Define Square class"""
+
+
+class Square:
+    """Definition of a square
+    Attributes:
+        __size (int): size of the square sides.
+    """
+    def __init__(self, size=0):
+        """Initialization of square
+        Args:
+            size (int): size of the square sides
+        Returns: None
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
