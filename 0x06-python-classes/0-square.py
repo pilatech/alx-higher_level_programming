ee2371a1ec3611009a9ece6f1414ddd34cfb479e
Pilate <pilatech6@gmail.com>
2023-10-25 02:41:10 +0200

    add #1 of 0x06 + docstring

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines Square class."""
+
+
 class Square:
     """An empty class for demonstration purposes."""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""Defines Square class"""
+
+
 class Square:
-    """Definition of a square with private instance attribute"""
+    """Definition of a square with private instance attribute
+    Attributes:
+        __size (int): size of the square sides.
+    """     
     def __init__(self, size):
+        """Initialization of square
+        Args:
+            size (int): size of the square sides
+        Return: None
+        """
         self.__size = size
