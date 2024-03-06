e62ee00a037cac5832adad35dc79bd24eb8a4ff7
Pilate <pilatech6@gmail.com>
2024-03-06 10:06:43 +0200

    add #10 of 0x0A

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module impleements Square class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Implements the Square shape """
+    def __init__(self, size):
+        """initializ the Rectangle shape
+         Args:
+            size: the size of the square sides
+        Raises: validation Exceptions on failure
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        Rectangle.__init__(self, size, size)
+
+    def area(self):
+        """Calculate the area of the Square
+        Returns: area of the square
+        """
+        return self.__size * self.__size
