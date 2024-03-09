15c20b072691db21b131f9acbdeab07967a9ae4d
Pilate <pilatech6@gmail.com>
2024-03-09 15:54:44 +0200

    edit #7 of 0x0B #1

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,11 +2,19 @@
 """Module for adding all arguments to Python list and saving to file"""
 import sys
 import json
+import os
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-obj = sys.argv[1:]
-save_to_json_file(obj, 'add_item.json')
-obj = load_from_json_file('add_item.json')
-print(obj)
+
+f_ = 'add_item.json'
+
+with open(f_, "a", encoding="utf-8"):
+    pass
+obj = load_from_json_file(f_) or []
+obj = obj + sys.argv[1:]
+save_to_json_file(obj, f_)
+with open(f_, 'r', encoding="utf-8") as f:
+    data = f.read()
+    print(data)
