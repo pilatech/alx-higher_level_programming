e38f8c37ae680ffe8c757395f75826f536c1ac2a
Pilate <pilatech6@gmail.com>
2024-03-09 18:46:44 +0200

    edit #7 of 0x0B #2

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,14 +7,15 @@ import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
 f_ = 'add_item.json'
 
-with open(f_, "a", encoding="utf-8"):
-    pass
-obj = load_from_json_file(f_) or []
-obj = obj + sys.argv[1:]
+if os.path.isfile(f_):
+    data = load_from_json_file(f_)
+    obj = list(data) + sys.argv[1:]
+else:
+    with open(f_, "w", encoding="utf-8"):
+        pass
+    obj = sys.argv[1:]
+
 save_to_json_file(obj, f_)
-with open(f_, 'r', encoding="utf-8") as f:
-    data = f.read()
-    print(data)
+data = load_from_json_file(f_)
