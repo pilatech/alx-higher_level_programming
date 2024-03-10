fbad76473e61c14449edfb3d60c31f2c9c92538d
Pilate <pilatech6@gmail.com>
2024-03-10 08:56:04 +0200

    add #11 of 0x0B

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Module for Student class"""
+
+
+class Student:
+    """Defines a Student object"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize the Student object
+        Args:
+            first_name: the student's first name
+            last_name: the student's last name
+            age: the student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Turns instances of the student to JSON
+        Return: A serializable object (dict)
+        """
+        if attrs:
+            dc = {}
+            for item in attrs:
+                if item in vars(self) and type(item) is str:
+                    dc[item] = vars(self)[item]
+            return dc
+        return vars(self)
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student object
+        Args:
+            json: a dictionary to replace with
+        """
+        self.__dict__ = json
