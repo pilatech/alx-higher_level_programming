91a39fd001ea1f44375f5765b58dac2c47dc7c16
Pilate <pilatech6@gmail.com>
2024-08-17 12:32:39 +0200

    edit_2 #8 0x0E

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,12 +1,11 @@
 -- Script to list cities in cities table using subquery
 
 -- list the cities
-SELECT *
+SELECT id, name
   FROM cities
  WHERE state_id = (
 	SELECT id
 	  FROM states
 	 WHERE name = 'California'
-	 LIMIT 1
 	)
 ORDER BY cities.id;
