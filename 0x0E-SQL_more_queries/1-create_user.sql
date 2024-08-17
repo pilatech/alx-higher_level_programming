73877a37c0d1e8d140637680a44354e3d6bf4286
Pilate <pilatech6@gmail.com>
2024-08-17 08:23:04 +0200

    add #2 of 0x0E

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,7 +3,7 @@
 -- Create user `user_0d_1` with all privileges
   CREATE USER 
 IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwdD';
+IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to the newly created user
 GRANT ALL PRIVILEGES
